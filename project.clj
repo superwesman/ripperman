f9e1c80734115607dab89315fff6a7947ea9ea8d
superwesman <superwesman@users.noreply.github.com>
2020-02-05 21:50:18 -0600

    recreated project clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject super/ripperman "0.1.0-SNAPSHOT"
+  :description "clojure wrapper around MakeMkv command-line interface (makemkvcon)"
+
+  :license {}
+
+  :dependencies [[com.taoensso/timbre "4.10.0"]
+
+                 [me.raynes/fs "1.4.6"]
+
+                 [org.clojure/clojure "1.10.0"]])
